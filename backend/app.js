ae51903509ee8c9d0c213be683da4ba602ab74cc
joao <joaogomes2609@gmail.com>
2026-05-13 12:39:27 +0100

    Remover ficheiros e dados nao utilizados

--- backend/app.js
+++ backend/app.js
@@ -24,18 +24,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/places", async (req, res) => {
-  const fileContent = await fs.readFile(dataPath("places.json"));
-  const placesData = JSON.parse(fileContent);
-  res.status(200).json({ places: placesData });
-});
-       
-app.get("/user-places", async (req, res) => {
-  const fileContent = await fs.readFile(dataPath("user-places.json"));
-  const places = JSON.parse(fileContent);
-  res.status(200).json({ places });
-});
-
 app.get("/users", async (req, res) => {
   const fileContent = await fs.readFile(dataPath("users.json"));
   const users = JSON.parse(fileContent);
@@ -52,12 +40,6 @@ app.get("/orders", async (req, res) => {
   const fileContent = await fs.readFile(dataPath("orders.json"));
   const orders = JSON.parse(fileContent);
   res.status(200).json({ orders });
-});
-
-app.put("/user-places", async (req, res) => {
-  const places = req.body.places;
-  await fs.writeFile(dataPath("user-places.json"), JSON.stringify(places));
-  res.status(200).json({ message: "User places updated!" });
 });
 
 //rotas de users
